5e0adadd3d1803cdddb6a432abcbef9f371fdedd
Jeddix <2885473+Jeddix@users.noreply.github.com>
2020-02-21 21:18:05 +0100

    AllTracksPlayedSimulation.py: Added graph plotting

--- AllTracksPlayedSimulation.py
+++ AllTracksPlayedSimulation.py
@@ -1,8 +1,11 @@
 import datetime
 import random
+import matplotlib.pyplot as plt 
 
-def printStats(playtime, playedSongs, perc):
+def generateAndPrintStats(playtime, playedSongs, perc, playtimesForGraph, numberOfSongsForGraph):
     print(str(round(playtime)) + ";" + str(playedSongs) + ";" + str(round(perc, 2)))
+    playtimesForGraph.append(playtime / 1000)
+    numberOfSongsForGraph.append(numberOfPlayedSongs)
 
 numberOfSongs = 8226
 playlistLengthInMinutes = 33846
@@ -11,6 +14,9 @@ playedSongs = [0] * numberOfSongs
 numberOfPlayedSongs = 0
 counter = 0
 playtime = 0
+
+playtimesForGraph = []
+numberOfSongsForGraph = []
 
 averageSongDuration = playlistLengthInMinutes / numberOfSongs
 
@@ -27,6 +33,14 @@ while numberOfPlayedSongs < numberOfSongs:
 
     if counter == 50:
         counter = 0
-        printStats(playtime, numberOfPlayedSongs, percentage)
+        generateAndPrintStats(playtime, numberOfPlayedSongs, percentage, playtimesForGraph, numberOfSongsForGraph)
 
-printStats(playtime, numberOfPlayedSongs, percentage)
+generateAndPrintStats(playtime, numberOfPlayedSongs, percentage, playtimesForGraph, numberOfSongsForGraph)
+
+plt.plot(numberOfSongsForGraph, playtimesForGraph) 
+  
+plt.xlabel('Anzahl Songs') 
+plt.ylabel('Spielzeit in 1000 Minuten')
+plt.title('Spielzeit bis alle Songs in Random abgespielt wurden')
+
+plt.show() 
